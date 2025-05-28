5391a77c178ec20059b941215a41779e80e477f7
nightclub.gm <kevincenteno39@gmail.com>
2025-05-28 01:48:37 -0500

    efec

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import StakingModal from "../components/StakingModal";
 import MissionModal from "../components/MissionModal";
 
 export default function Home() {
@@ -10,14 +9,15 @@ export default function Home() {
       <h1 className="gigaland-title text-center mb-8 mt-12 text-3xl font-bold text-white">
         Night Club Game
       </h1>
-      <StakingModal />
       <button
         className="mt-6 bg-pink-600 text-white py-3 px-6 rounded-xl font-bold shadow-lg hover:bg-pink-700"
         onClick={() => setShowMissionModal(true)}
       >
         Seleccionar Misión
       </button>
-      {showMissionModal && <MissionModal onClose={() => setShowMissionModal(false)} />}
+      {showMissionModal && (
+        <MissionModal onClose={() => setShowMissionModal(false)} />
+      )}
     </div>
   );
 }
