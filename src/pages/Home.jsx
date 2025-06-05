c162836665c720b8a038dd23112b747ab11556da
nightclub.gm <kevincenteno39@gmail.com>
2025-06-05 15:08:26 -0500

    cece

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
               <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="#ff36ba" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
             </button>
           </div>
-          <img src="/mapa1.svg" alt="Mapa" className="home-image" />
+          <img src="/mapa1.png" alt="Mapa" className="home-image" />
           <div
             className="mission-button edificio1-map"
             onClick={() => setShowMission(true)}
